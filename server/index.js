05cac93a9a047697d762e3dd5b7a6251df04bd77
Indragie Karunaratne <i@indragie.com>
2016-01-30 14:27:42 -0700

    Implemented POST /authenticate

--- server/index.js
+++ server/index.js
@@ -5,11 +5,23 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var async = require('async');
 var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+// ######## CONSTANTS #########
+
+USERS_COLLECTION = 'users';
+TOKEN_EXPIRY_SECONDS = 86400; // 24 hours
+ERROR_USER_EXISTS = new Error('USER_EXISTS');
+ERROR_USER_DOES_NOT_EXIST = new Error('USER_DOES_NOT_EXIST');
+ERROR_PASSWORD_INCORRECT = new Error('PASSWORD_INCORRECT');
 
 // ######## EXPRESS #########
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.set('port', (process.env.PORT || 5000));
+app.set('mongo_uri', process.env.MONGOLAB_URI);
+app.set('secret', process.env.APP_SECRET);
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
@@ -25,32 +37,82 @@ app.post('/register', function(req, res, next) {
         async.apply(createUser, req.body.username, req.body.password)
     ], function(err, result) {
         if (err) {
-            res.status(400).json({ error: err.message });
+            return next(err);
         } else {
-            res.json(result.ops[0]);
+            res.json({ 
+                success: true, 
+                user: result.ops[0]
+            });
         }
+    });
+});
+
+app.post('/authenticate', function(req, res, next) {
+    async.waterfall([
+        connectMongoDB,
+        async.apply(getUser, req.body.username),
+        function(user, callback) {
+            if (user) {
+                callback(null, user);
+            } else {
+                callback(ERROR_USER_DOES_NOT_EXIST);
+            }
+        },
+        function(user, callback) {
+            verifyPassword(user.password_hash, req.body.password, function(err, res) {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, res, user);
+                }
+            });
+        },
+        function(verified, user, callback) {
+            if (verified) {
+                callback(null, user);
+            } else {
+                callback(ERROR_PASSWORD_INCORRECT);
+            }
+        },
+    ], function(err, user) {
+        if (err) {
+            return next(err);
+        } else {
+            var token = jwt.sign(user, app.get('secret'), {
+                expiresIn: TOKEN_EXPIRY_SECONDS
+            });
+            res.json({
+                success: true,
+                token: token
+            });
+        }
+    });
+});
+
+app.use(function(err, req, res, next) {
+    res.status(400).json({ 
+        success: false,
+        error: err.message 
     });
 });
 
 // ######## MONGODB #########
 
-USERS_COLLECTION = "users";
-
 var connectMongoDB = function(callback) {
-    MongoClient.connect(process.env.MONGOLAB_URI, callback);
+    MongoClient.connect(app.get('mongo_uri'), callback);
 };
 
-var getUserWithUsername = function(username, db, callback) {
+var getUser = function(username, db, callback) {
     var users = db.collection(USERS_COLLECTION);
     users.findOne({ username: username }, callback);
 };
 
 var createUser = function(username, password, db, finalCallback) {
     async.waterfall([
-        async.apply(getUserWithUsername, username, db),
+        async.apply(getUser, username, db),
         function(user, callback) {
             if (user) {
-                callback(new Error('USER_EXISTS'));
+                callback(ERROR_USER_EXISTS);
             } else {
                 callback(null, password);
             }
@@ -65,8 +127,14 @@ var createUser = function(username, password, db, finalCallback) {
     ], finalCallback);
 };
 
+// ######## HASHING #########
+
 var hashPassword = function(password, callback) {
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(password, salt, callback);
     });
+};
+
+var verifyPassword = function(hash, password, callback) {
+    bcrypt.compare(password, hash, callback);
 };
