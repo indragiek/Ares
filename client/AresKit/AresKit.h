6fdc87ebae1112c6041fbf3ad86bd62c5eae89bf
Indragie Karunaratne <i@indragie.com>
2016-01-30 12:18:09 -0700

    Create iOS and Mac targets

---
+++ client/AresKit/AresKit.h
@@ -0,0 +1,19 @@
+//
+//  AresKit.h
+//  AresKit
+//
+//  Created by Indragie on 1/30/16.
+//  Copyright © 2016 Indragie Karunaratne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AresKit.
+FOUNDATION_EXPORT double AresKitVersionNumber;
+
+//! Project version string for AresKit.
+FOUNDATION_EXPORT const unsigned char AresKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AresKit/PublicHeader.h>
+
+
