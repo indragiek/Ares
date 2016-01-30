62c26abe4038320e679a397ef293ac23443ab53d
Indragie Karunaratne <i@indragie.com>
2016-01-30 12:44:15 -0700

    Set up MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+// ######## EXPRESS #########
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,5 +12,13 @@ app.get('/', function (req, res) {
 });
 
 app.listen(app.get('port'), function () {
-  console.log('Example server started');
+    console.log('Example server started');
+});
+
+// ######## MONGODB #########
+
+MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
+    assert.equal(null, err);
+    console.log('Successfully connected to MongoDB');
+    db.close();
 });
